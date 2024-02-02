9a930d68d9d72ad91bbd6990a9a227cab836b1e4
Sketch <75850871+SketchMaster2001@users.noreply.github.com>
2024-02-02 17:17:06 -0500

    Fix sending

--- send.go
+++ send.go
@@ -13,8 +13,8 @@ var (
 	sendAuthRegex    = regexp.MustCompile(`^mlid=(w\d{16})\r?\npasswd=(.{16,32})$`)
 	mailFormKeyRegex = regexp.MustCompile(`m\d+`)
 	recipientRegex   = regexp.MustCompile(`^RCPT TO:\s(.*)@(.*)$`)
-	fromRegex        = regexp.MustCompile(`^MAIL FROM:\s(.*)@mail.wiilink24.com$`)
-	fromRegexPayload = regexp.MustCompile(`^From:\s(.*)@mail.wiilink24.com$`)
+	fromRegex        = regexp.MustCompile(`^MAIL FROM:\s(.*)@rc24.xyz$`)
+	fromRegexPayload = regexp.MustCompile(`^From:\s(.*)@rc24.xyz$`)
 )
 
 const (
@@ -93,16 +93,16 @@ func send(r *Response) string {
 
 			recipientMatch := recipientRegex.FindStringSubmatch(line)
 			if recipientMatch != nil {
-				if recipientMatch[2] == "wii.com" || recipientMatch[2] == "rc24.xyz" {
+				if recipientMatch[2] == "wii.com" || recipientMatch[2] == "mail.wiilink24.com" {
 					// Theoretically this should not be possible.
 					// A message formulated by a Wii used the address found in nwc24msg.cfg.
-					// If we got far, it would be @mail.wiilink24.com.
+					// If we got far, it would be @rc24.xyz.
 					// Regardless, if this does happen we don't want it clogging up our database or wasting
 					// precious API calls.
 
 					// Going back to my second comment, there was a moment where an attacker had the recipient
 					// as RC24, causing it to spam both our clients. As such we should block any RC24 recipients.
-				} else if recipientMatch[2] == "mail.wiilink24.com" {
+				} else if recipientMatch[2] == "rc24.xyz" {
 					wiiRecipients = append(wiiRecipients, recipientMatch[1])
 				} else {
 					// This is an email.
@@ -133,7 +133,7 @@ func send(r *Response) string {
 		// Format: w9004342343324713@wii.com <mailto:w9004342343324713@wii.com>
 		parsedMail = strings.Replace(parsedMail,
 			fmt.Sprintf("%s@wii.com <mailto:%s@wii.com>", mlid, mlid),
-			fmt.Sprintf("%s@mail.wiilink24.com <mailto:%s@mail.wiilink24.com>", mlid, mlid),
+			fmt.Sprintf("%s@rc24.xyz <mailto:%s@rc24.xyz>", mlid, mlid),
 			-1)
 
 		var didError bool
@@ -164,11 +164,11 @@ func send(r *Response) string {
 		for _, recipient := range emailRecipients {
 			// PC Mail
 			// We currently utilize SendGrid, TODO: Use MailGun we get 20k messages/month
-			auth := smtp.PlainAuth("", "postmaster@mail.wiilink24.com", config.MailGunKey, "smtp.mailgun.org")
+			auth := smtp.PlainAuth("", "postmaster@rc24.xyz", config.MailGunKey, "smtp.mailgun.org")
 			err = smtp.SendMail(
 				"smtp.mailgun.org:587",
 				auth,
-				fmt.Sprintf("%s@mail.wiilink24.com", mlid),
+				fmt.Sprintf("%s@rc24.xyz", mlid),
 				[]string{recipient},
 				[]byte(parsedMail),
 			)
