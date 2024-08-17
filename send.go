7e7f655eef450ecd0fae160a8b24902797bc5ca0
Sketch <75850871+SketchMaster2001@users.noreply.github.com>
2024-08-17 15:50:57 -0400

    fix: Open less connections

--- send.go
+++ send.go
@@ -10,7 +10,6 @@ import (
 	"net/smtp"
 	"regexp"
 	"strings"
-	"sync"
 )
 
 var (
@@ -27,10 +26,11 @@ const (
 )
 
 func send(c *gin.Context) {
+	ctx := c.Copy()
 	c.Header("Content-Type", "text/plain;charset=utf-8")
 
 	mlid, password := parseSendAuth(c.PostForm("mlid"))
-	err := validatePassword(c.Copy(), mlid, password)
+	err := validatePassword(ctx, mlid, password)
 	if errors.Is(err, ErrInvalidCredentials) {
 		cgi := GenCGIError(250, err.Error())
 		c.String(http.StatusOK, ConvertToCGI(cgi))
@@ -67,133 +67,139 @@ func send(c *gin.Context) {
 	cgi.code = 100
 	cgi.message = "Success."
 
-	wg := &sync.WaitGroup{}
-	mu := &sync.Mutex{}
-
 	for index, content := range mails {
-		wg.Add(1)
-		go func(index string, content string) {
-			defer wg.Done()
-			var wiiRecipients []string
-			var emailRecipients []string
-			var hasError bool
+		var wiiRecipients []string
+		var emailRecipients []string
+		var hasError bool
 
-			// Read line by line
-			msgIndex := 0
-			scanner := bufio.NewScanner(strings.NewReader(content))
-			for scanner.Scan() {
-				line := scanner.Text()
+		// Read line by line.
+		// If you look in Git history, you can see that I used a method that was faster than this current one,
+		// then I learnt Wii Speak is able to send to multiple recipients at once.
+		msgIndex := 0
+		scanner := bufio.NewScanner(strings.NewReader(content))
+		for scanner.Scan() {
+			line := scanner.Text()
 
-				if line == "DATA" {
-					// Find the index of the actual data and skip to the end
-					scanner.Scan()
-					line = scanner.Text()
-					msgIndex = strings.Index(content, line)
-					continue
-				}
+			if line == "DATA" {
+				// We have reached the end of metadata parsing.
+				// Find the index of the actual data and skip to the end.
+				scanner.Scan()
+				line = scanner.Text()
+				msgIndex = strings.Index(content, line)
+				continue
+			}
 
-				if senderMatch := fromRegex.FindStringSubmatch(line); senderMatch != nil {
-					if senderMatch[1] != mlid {
-						mu.Lock()
-						cgi.AddMailResponse(index, 350, "Attempted to impersonate another user.")
-						mu.Unlock()
-						hasError = true
-						break
-					}
-					continue
-				}
-
-				if recipientMatch := recipientRegex.FindStringSubmatch(line); recipientMatch != nil {
-					if recipientMatch[2] == "rc24.xyz" {
-						wiiRecipients = append(wiiRecipients, recipientMatch[1])
-					} else {
-						emailRecipients = append(emailRecipients, fmt.Sprintf("%s@%s", recipientMatch[1], recipientMatch[2]))
-					}
-				}
-
-				if senderMatch := fromRegexPayload.FindStringSubmatch(line); senderMatch != nil {
-					if senderMatch[1] != mlid {
-						mu.Lock()
-						cgi.AddMailResponse(index, 350, "Attempted to impersonate another user.")
-						mu.Unlock()
-						hasError = true
-					}
+			senderMatch := fromRegex.FindStringSubmatch(line)
+			if senderMatch != nil {
+				if senderMatch[1] != mlid {
+					cgi.AddMailResponse(index, 350, "Attempted to impersonate another user.")
+					hasError = true
 					break
+				}
+				continue
+			}
+
+			recipientMatch := recipientRegex.FindStringSubmatch(line)
+			if recipientMatch != nil {
+				if recipientMatch[2] == "wii.com" || recipientMatch[2] == "mail.wiilink24.com" {
+					// Theoretically this should not be possible.
+					// A message formulated by a Wii used the address found in nwc24msg.cfg.
+					// If we got far, it would be @rc24.xyz.
+					// Regardless, if this does happen we don't want it clogging up our database or wasting
+					// precious API calls.
+
+					// Going back to my second comment, there was a moment where an attacker had the recipient
+					// as WiiLink, causing it to spam both our clients. As such we should block any WiiLink recipients.
+				} else if recipientMatch[2] == "rc24.xyz" {
+					wiiRecipients = append(wiiRecipients, recipientMatch[1])
+				} else {
+					// This is an email.
+					emailRecipients = append(emailRecipients, fmt.Sprintf("%s@%s", recipientMatch[1], recipientMatch[2]))
 				}
 			}
 
-			if hasError {
-				return
+			// Additionally we also check the actual mail to see who it is coming from. A malicious user could
+			// send a correct header, but spoof the actual mail, impersonating another user
+			senderMatch = fromRegexPayload.FindStringSubmatch(line)
+			if senderMatch != nil {
+				if senderMatch[1] != mlid {
+					cgi.AddMailResponse(index, 350, "Attempted to impersonate another user.")
+					hasError = true
+				}
+				break
+			}
+		}
+
+		if hasError {
+			continue
+		}
+
+		parsedMail := content[msgIndex:]
+
+		// Replace all @wii.com references in the
+		// friend request email with our own domain.
+		// Format: w9004342343324713@wii.com <mailto:w9004342343324713@wii.com>
+		parsedMail = strings.Replace(parsedMail,
+			"wii.com",
+			"rc24.xyz",
+			-1)
+
+		var didError bool
+		for _, recipient := range wiiRecipients {
+			var exists bool
+			err := pool.QueryRow(ctx, RecipientExists, recipient[1:]).Scan(&exists)
+			if err != nil && !errors.Is(err, sql.ErrNoRows) {
+				cgi.AddMailResponse(index, 551, "Issue verifying recipient.")
+				ReportError(err)
+				didError = true
+				break
+			} else if !exists {
+				// Account doesn't exist, ignore
+				didError = true
+				break
 			}
 
-			parsedMail := content[msgIndex:]
+			// Finally insert!
+			_, err = pool.Exec(ctx, InsertMail, flakeNode.Generate(), parsedMail, mlid[1:], recipient[1:])
+			if err != nil {
+				cgi.AddMailResponse(index, 450, "Database error.")
+				ReportError(err)
+				didError = true
+				break
+			}
+		}
 
-			parsedMail = strings.Replace(parsedMail,
-				"wii.com",
-				"rc24.xyz",
-				-1)
+		for _, recipient := range emailRecipients {
+			// PC Mail
+			// We currently utilize SendGrid.
+			auth := smtp.PlainAuth("", "apikey", config.SendGridKey, "smtp.sendgrid.net")
+			err = smtp.SendMail(
+				"smtp.sendgrid.net:587",
+				auth,
+				fmt.Sprintf("%s@rc24.xyz", mlid),
+				[]string{recipient},
+				[]byte(parsedMail),
+			)
+			if err != nil {
+				cgi.AddMailResponse(index, 551, "Sendgrid error.")
+				// ReportError(err)
+				didError = true
+				continue
+			}
+		}
 
-			var didError bool
-			for _, recipient := range wiiRecipients {
-				var exists bool
-				err := pool.QueryRow(c.Copy(), RecipientExists, recipient[1:]).Scan(&exists)
-				if err != nil && !errors.Is(err, sql.ErrNoRows) {
-					mu.Lock()
-					cgi.AddMailResponse(index, 551, "Issue verifying recipient.")
-					mu.Unlock()
-					ReportError(err)
-					didError = true
-					break
-				} else if !exists {
-					didError = true
-					break
-				}
+		if !didError {
+			// If everything was successful we write that to the response.
+			cgi.AddMailResponse(index, 100, "Success.")
 
-				_, err = pool.Exec(c.Copy(), InsertMail, flakeNode.Generate(), parsedMail, mlid[1:], recipient[1:])
+			if config.UseDatadog {
+				err = dataDog.Incr("mail.sent_mail", nil, 1)
 				if err != nil {
-					mu.Lock()
-					cgi.AddMailResponse(index, 450, "Database error.")
-					mu.Unlock()
 					ReportError(err)
-					didError = true
-					break
 				}
 			}
-
-			if !didError {
-				for _, recipient := range emailRecipients {
-					auth := smtp.PlainAuth("", "apikey", config.SendGridKey, "smtp.sendgrid.net")
-					err := smtp.SendMail(
-						"smtp.sendgrid.net:587",
-						auth,
-						fmt.Sprintf("%s@rc24.xyz", mlid),
-						[]string{recipient},
-						[]byte(parsedMail),
-					)
-					if err != nil {
-						mu.Lock()
-						cgi.AddMailResponse(index, 551, "Sendgrid error.")
-						mu.Unlock()
-						didError = true
-					}
-				}
-			}
-
-			if !didError {
-				mu.Lock()
-				cgi.AddMailResponse(index, 100, "Success.")
-				mu.Unlock()
-				if config.UseDatadog {
-					err = dataDog.Incr("mail.sent_mail", nil, 1)
-					if err != nil {
-						ReportError(err)
-					}
-				}
-			}
-		}(index, content)
+		}
 	}
-
-	wg.Wait()
 
 	c.String(http.StatusOK, ConvertToCGI(*cgi))
 }
